db13a101a151620aaef9a0999147c990246004e4
no-rahc <167065250+no-rahc@users.noreply.github.com>
2026-08-05 13:35:55 +0900

    build(ui): load compact dashboard assets

--- app_entry.py
+++ app_entry.py
@@ -153,6 +153,9 @@ class ConsoleAssetsMiddleware(BaseHTTPMiddleware):
             '<link rel="stylesheet" href="/static/css/app-v3.css?v='
             + version
             + '" data-lar-ui-v3>'
+            '<link rel="stylesheet" href="/static/css/dashboard-glance-v1.css?v='
+            + version
+            + '" data-lar-dashboard-glance-v1>'
             '<link rel="stylesheet" href="/static/css/recording-density-v1.css?v='
             + version
             + '" data-lar-recording-density-v1>'
