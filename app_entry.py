bfe4e31f72137dd688cbf0a5c5a2c88b9bc0d389
no-rahc <167065250+no-rahc@users.noreply.github.com>
2026-08-05 10:15:36 +0900

    Load redesigned shell and dashboard assets

--- app_entry.py
+++ app_entry.py
@@ -62,12 +62,18 @@ class ConsoleAssetsMiddleware(BaseHTTPMiddleware):
             '<link rel="stylesheet" href="/static/css/console-v2.css?v='
             + version
             + '" data-lar-console-v2>'
+            '<link rel="stylesheet" href="/static/css/shell-v3.css?v='
+            + version
+            + '" data-lar-shell-v3>'
             '<link rel="stylesheet" href="/static/css/sidebar-v3.css?v='
             + version
             + '" data-lar-sidebar-v3>'
             '<link rel="stylesheet" href="/static/css/system-metrics-v2.css?v='
             + version
             + '" data-lar-metrics-v2>'
+            '<link rel="stylesheet" href="/static/css/dashboard-v4.css?v='
+            + version
+            + '" data-lar-dashboard-v4>'
             '<script src="/static/js/system-metrics-v2.js?v='
             + version
             + '" data-lar-metrics-v2></script>'
@@ -79,6 +85,9 @@ class ConsoleAssetsMiddleware(BaseHTTPMiddleware):
             '<script src="/static/js/sidebar-v3.js?v='
             + version
             + '" defer data-lar-sidebar-v3></script>'
+            '<script src="/static/js/dashboard-v4.js?v='
+            + version
+            + '" defer data-lar-dashboard-v4></script>'
         )
 
         if "data-lar-console-v2" not in html:
